fa42d3a714f51398279d062e9d33dfd0d8c36d60
Ankit Singh <ankitmashu@gmail.com>
2022-05-04 00:44:04 +0530

    4/5/22

---
+++ src/test/java/com/in28minutes/mockito/FirstMockitoTest.java
@@ -0,0 +1,11 @@
+package com.in28minutes.mockito;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class FirstMockitoTest {
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
